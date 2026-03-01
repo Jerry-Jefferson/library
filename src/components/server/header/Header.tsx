c722629ed1fd67bf0ff591e85e53242eafb15544
Jerry-Jefferson <karzovsnastassia@gmail.com>
2026-03-01 18:43:07 +0300

    Import change

--- src/components/server/header/Header.tsx
+++ src/components/server/header/Header.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/src/auth";
 import { NAV_LINKS_ROLE, SessionRoles } from "@/src/shared/constants/navigationLinks";
 import { roles } from "@/src/shared/constants/roles";
-import HeaderView from "../../client/headerView/headerView";
+import HeaderView from "../../client/headerView/HeaderView";
 
 export async function Header() {
   const session = await auth();
