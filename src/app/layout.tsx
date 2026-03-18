94dcd9accec4dd01a4a8b609f77df1384a4727b2
cynzlic <e.yushkevich30@gmail.com>
2026-03-18 23:51:45 +0300

    remove icon from metadata object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Your library",
   description: "Here you can see books and authors",
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
