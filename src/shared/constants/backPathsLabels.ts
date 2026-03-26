b600604382db3bbf6fcd1f1ee884932c7699fb86
Jerry-Jefferson <karzovsnastassia@gmail.com>
2026-03-26 17:31:24 +0300

    small change in back paths constant

--- src/shared/constants/backPathsLabels.ts
+++ src/shared/constants/backPathsLabels.ts
@@ -3,7 +3,6 @@ import { routes } from "./routes";
 export const BACK_PATHS_LABELS: Record<string, string> = {
   [routes.books]: "Book Directory",
   [routes.home]: "Home",
-  [routes.authors]: "Author Directory",
 };
 
 export const DEFAULT_LABEL = "Previous";
