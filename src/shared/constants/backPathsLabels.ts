032d6962105997dbbf6971fc98341dd9eeb0c780
Jerry-Jefferson <karzovsnastassia@gmail.com>
2026-03-26 15:44:03 +0300

    Refactor back path button

---
+++ src/shared/constants/backPathsLabels.ts
@@ -0,0 +1,9 @@
+import { routes } from "./routes";
+
+export const BACK_PATHS_LABELS: Record<string, string> = {
+  [routes.books]: "Book Directory",
+  [routes.home]: "Home",
+  [routes.authors]: "Author Directory",
+};
+
+export const DEFAULT_LABEL = "Previous";
