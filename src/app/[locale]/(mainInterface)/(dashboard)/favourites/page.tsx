a25e01c5bfc0295478d8b7a7a6ce246b21e36b54
Jerry-Jefferson <karzovsnastassia@gmail.com>
2026-05-19 22:28:00 +0300

    pages changes

--- src/app/[locale]/(mainInterface)/(dashboard)/favourites/page.tsx
+++ src/app/[locale]/(mainInterface)/(dashboard)/favourites/page.tsx
@@ -1,28 +1,29 @@
+import { ItemsSkeleton } from "@/src/app/(mainInterface)/components/itemsSkeleton/itemsSkeleton";
+import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
 import { FavoritesList } from "./components/favoritesList";
-import { getTranslations } from "next-intl/server";
 
 export default async function Favourites() {
-  const tCommon = await getTranslations("Common");
-  const tEntity = await getTranslations("Entities");
   const t = await getTranslations("Favorites");
+
   return (
-    <Suspense
-      fallback={
-        <p>
-          {tCommon("loading", {
-            entity: tEntity("favorites.loading"),
-          })}
-        </p>
-      }
-    >
-      <div className="w-full flex justify-center bg-background">
-        <div className="w-7/8 gap-4 flex flex-col mt-10 mb-10">
-          <h2 className="text-5xl sm:text-6xl font-bold">{t("masterpieces")}</h2>
-          <p className="text-lg sm:text-xl text-secondary">{t("description")}</p>
+    <div className="w-full flex justify-center bg-background">
+      <div className="w-7/8 gap-4 flex flex-col mt-10 mb-10">
+        <h2 className="text-5xl sm:text-6xl font-bold">{t("masterpieces")}</h2>
+        <p className="text-lg sm:text-xl text-secondary">{t("description")}</p>
+        <Suspense
+          fallback={
+            <ItemsSkeleton
+              className="h-[580px]"
+              gridClassName="grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+              itemsNumber={8}
+              withSort={false}
+            />
+          }
+        >
           <FavoritesList />
-        </div>
+        </Suspense>
       </div>
-    </Suspense>
+    </div>
   );
 }
