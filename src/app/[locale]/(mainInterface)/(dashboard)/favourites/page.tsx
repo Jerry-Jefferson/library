f7aa9eed1c593b5575ccf1e1c6c2cec5e49afd71
Jerry-Jefferson <karzovsnastassia@gmail.com>
2026-05-21 14:43:18 +0300

    Add loading file for favorites, change masterpieces to saved masterpieces, change plurality of favorites in russian translation

--- src/app/[locale]/(mainInterface)/(dashboard)/favourites/page.tsx
+++ src/app/[locale]/(mainInterface)/(dashboard)/favourites/page.tsx
@@ -1,11 +1,9 @@
-import { getTranslations } from "next-intl/server";
 import { getFavoriteBooks } from "@/lib/modules/books/books";
 import { auth } from "@/src/auth";
 import EmptyState from "@/src/components/client/emptyState/emptyState";
-import BooksList from "./components/booksList";
 import { routes } from "@/src/shared/constants/routes";
-import { Suspense } from "react";
-import { ItemsSkeleton } from "../../components/itemsSkeleton/itemsSkeleton";
+import { getTranslations } from "next-intl/server";
+import BooksList from "./components/booksList";
 
 interface FavoritesProps {
   params: Promise<{ locale: string }>;
@@ -39,18 +37,7 @@ export default async function Favourites({ params }: FavoritesProps) {
             buttonLabel={t("toBookPage")}
           />
         ) : (
-          <Suspense
-            fallback={
-              <ItemsSkeleton
-                className="h-[580px]"
-                gridClassName="grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-                itemsNumber={8}
-                withSort={false}
-              />
-            }
-          >
-            <BooksList books={favoriteBooks} />
-          </Suspense>
+          <BooksList books={favoriteBooks} />
         )}
       </div>
     </div>
